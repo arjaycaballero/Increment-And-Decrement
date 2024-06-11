edc8db5bbb76f62539d0d9b3a7471f8035206a9f
Arjay Caballero <130941463+arjaycaballero@users.noreply.github.com>
2024-06-11 14:32:19 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/* var x = 5;
+    x++; the answer is 6 and this is what you called increment
+
+    var y = 5;
+    y--; the answer is 4 and this is what you called decrement
+ */
+
+/* var x = 5;
+    x += 3; answer is 8 and this is the shorthand for increment with greater than 1.
+
+    var y = 5;
+    var x = 2;
+    x += y; the answer is 7
+
+    var x = 5;
+    x *= 3; the answer is 15.
+
+    var x = 10;
+    x /= 2; the answer is 5.
+*/
